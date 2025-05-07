94be69639fb70768f2178eb90bfa35a7117135f8
hoangmanhkhiem <khiemhm04@gmail.com>
2025-05-08 00:42:44 +0700

    create dashboard for role & change data for product

--- models/Product.ts
+++ models/Product.ts
@@ -3,8 +3,13 @@ import mongoose from "mongoose";
 // cong ty logictuc
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    owner: { type: String, required: true },
+    receiver: { type: String, required: true },
     addressWallet: { type: String, required: true },
     provider: { type: String, required: true },
+    dateAdded: { type: Date, default: Date.now },
+    status: { type: String, required: true },
+    type: { type: String, required: true }
 });
 
 const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
